0133dadc1e420c143e98cbcc57f76184547ff924
Justin Wayne Wilson <wilsonjustinwayne@gmail.com>
2018-08-12 15:57:18 -0500

    road trip changes

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,32 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav();
-});
+(function () {
+    "use strict";
+
+    const aboutMeButton = $('#about-meButton');
+    const aboutMePage = $('#about-meButton');
+    const proficiencyButton = $('#proficiencyButton');
+    const projectsButton = $('#projectsButton');
+    const bitButton = $('#bitButton');
+    const resumeButton = $('#resumeButton');
+    const contactButton = $('#contactButton');
+    const list = []
+    function switchTabs(visible, list) {
+    }
+
+    $(document).ready(function () {
+        $('.sidenav').sidenav();
+    });
+
+    aboutMeButton.click();
+
+    proficiencyButton.click();
+
+    projectsButton.click();
+
+    resumeButton.click();
+
+    contactButton.click();
+
+    bitButton.click(() => {
+        console.log("8-bit TIME!!!!!");
+    });
+})();
