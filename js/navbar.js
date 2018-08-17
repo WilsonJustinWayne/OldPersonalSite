bb8b423089a5f61c779d20dffce9f833a06c00ec
Justin Wayne Wilson <wilsonjustinwayne@gmail.com>
2018-08-17 11:17:21 -0500

    committing so website is set to main page while i work on changes

--- js/navbar.js
+++ js/navbar.js
@@ -2,31 +2,44 @@
     "use strict";
 
     const aboutMeButton = $('#about-meButton');
-    const aboutMePage = $('#about-meButton');
+    const aboutMePage = $('.about-me');
     const proficiencyButton = $('#proficiencyButton');
+    const proficiencyPage = $('.proficiency');
     const projectsButton = $('#projectsButton');
+    const projectsPage = $('.projects');
     const bitButton = $('#bitButton');
     const resumeButton = $('#resumeButton');
+    const resumePage = $('.resume');
     const contactButton = $('#contactButton');
-    const list = []
-    function switchTabs(visible, list) {
-    }
+    const contactPage = $('.contact');
+    const list = [aboutMePage, proficiencyPage, projectsPage, resumePage, contactPage];
+
+    // function switchTabs(visible/*, list*/) {
+        // console.log(list);
+
+        // for (let i = 0; i < list.length; i++) {
+            // console.log(list[i]);
+            // console.log("checking index " + i);
+            // console.log(list[i].name)
+            // if (!i.classList.contains("hide")) {
+            //     i.addClass("hide");
+            // }
+        // }
+        // visible.removeClass("hide");
+    // }
 
     $(document).ready(function () {
         $('.sidenav').sidenav();
     });
 
-    aboutMeButton.click();
-
-    proficiencyButton.click();
-
-    projectsButton.click();
-
-    resumeButton.click();
-
-    contactButton.click();
-
-    bitButton.click(() => {
-        console.log("8-bit TIME!!!!!");
-    });
+    // aboutMeButton.click(switchTabs(aboutMePage/*, list*/));
+    //
+    // proficiencyButton.click(switchTabs(proficiencyPage/*, list*/));
+    //
+    // projectsButton.click(switchTabs(projectsPage/*, list*/));
+    //
+    // resumeButton.click(switchTabs(resumePage/*, list*/));
+    //
+    // contactButton.click(switchTabs(contactPage/*, list*/));
+//
 })();
